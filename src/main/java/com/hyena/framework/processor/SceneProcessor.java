ccbe1f2ac27b41dc59fff2d8ab6323280235efcb
yangzc <zhc_yang@163.com>
2023-07-03 15:48:47 +0800

    dev

--- src/main/java/com/hyena/framework/processor/SceneProcessor.java
+++ src/main/java/com/hyena/framework/processor/SceneProcessor.java
@@ -7,28 +7,14 @@ import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Filer;
-import javax.annotation.processing.Messager;
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by yangzc on 17/11/23.
@@ -39,19 +25,20 @@ public class SceneProcessor extends AbstractProcessor {
 	private static final String APPLICATION_ID = "applicationId";
 
 	private Filer filer;
-	private Elements elementUtils;
+//	private Elements elementUtils;
 	private Messager messager;
 
 	private String applicationId = "com.hyena.ids";
-	private HashMap<String, String> mSceneMap = new HashMap<>();
+	private final HashMap<String, String> mSceneMap = new HashMap<>();
 
 	@Override
 	public synchronized void init(ProcessingEnvironment processingEnvironment) {
 		super.init(processingEnvironment);
 		this.filer = processingEnvironment.getFiler();
-		this.elementUtils = processingEnvironment.getElementUtils();
+//		this.elementUtils = processingEnvironment.getElementUtils();
 		this.messager = processingEnvironment.getMessager();
 
+		messager.printMessage(Diagnostic.Kind.NOTE, "init!!!");
 		if (processingEnvironment.getOptions() != null) {
 			Map<String, String> options = processingEnvironment.getOptions();
 			String applicationId = options.get(APPLICATION_ID);
@@ -74,12 +61,10 @@ public class SceneProcessor extends AbstractProcessor {
 			mSceneMap.put(scene.value(), e.toString());
 		}
 		TypeSpec.Builder javaClassBuilder = TypeSpec.classBuilder("SceneIds").addModifiers(Modifier.PUBLIC);
-		List<String> methodLine = new ArrayList();
+		List<String> methodLine = new ArrayList<>();
 		if (!mSceneMap.isEmpty()) {
 			//添加静态变量
-			Iterator<String> iterator = mSceneMap.keySet().iterator();
-			while (iterator.hasNext()) {
-				String sceneId = iterator.next();
+			for (String sceneId : mSceneMap.keySet()) {
 				String className = mSceneMap.get(sceneId);
 				FieldSpec fieldSpec = FieldSpec.builder(String.class, sceneId)
 						.addModifiers(Modifier.PUBLIC, Modifier.FINAL, Modifier.STATIC)
@@ -94,8 +79,8 @@ public class SceneProcessor extends AbstractProcessor {
 				.returns(HashMap.class)
 				.addModifiers(Modifier.STATIC, Modifier.PUBLIC)
 				.addStatement("$T sceneMap = new $T()", HashMap.class, HashMap.class);
-		for (int i = 0; i < methodLine.size(); i++) {
-			methodGetSceneMapSpecBuild.addStatement(methodLine.get(i));
+		for (String s : methodLine) {
+			methodGetSceneMapSpecBuild.addStatement(s);
 		}
 		methodGetSceneMapSpecBuild.addStatement("return sceneMap");
 		javaClassBuilder.addMethod(methodGetSceneMapSpecBuild.build());
